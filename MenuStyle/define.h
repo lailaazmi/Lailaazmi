06b7990837e6b80323f088716ef4052c3940fb34
Laila Azmi <lailaazmi@my.com>
2022-11-05 11:35:18 +0800

    lailaazmi

--- MenuStyle/define.h
+++
@@ -1,5 +0,0 @@
-#define PLUGINS_DIR_PATH @"/Library/MenuSupport/Plugins/"
-#define PREF_PATH @"/var/mobile/Library/Preferences/Lailaazmi.MenuStyle.plist"
-#define PLUGIN_ENABLED_PREFIX @"MSPluginEnabled"
-
-static CFStringRef const kNotificationName = CFSTR("com.lailaazmi.menustyle.ReloadPrefs");
