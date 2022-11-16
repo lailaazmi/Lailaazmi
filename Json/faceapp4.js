54b57cd786a9e95b0e779007d382b083ce8b83c3
Laila Azmi <lailaazmi@my.com>
2022-11-16 13:37:51 +0800

    lailaazmi

---
+++ Json/faceapp4.js
@@ -0,0 +1,14 @@
+var obj = JSON.parse($response.body);
+
+obj={
+  "subscription_apple" : {
+    "subscription_exp" : 1622610505,
+    "product_id" : "1"
+  },
+  "subscription_google" : null,
+  "user_token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJkZXZpY2VfaWQiOiIzMzhCMDA1Ny1BQUIzLTQ4OTYtODgxNS1GQTM2ODJBRTE3MTQiLCJleHAiOjE2MjI2MTA1MDUsInN1YnNjcmlwdGlvbiI6eyJzdWJzY3JpcHRpb25fZXhwIjoxNjIyNjEwNTA1LCJjdXN0b21lcl9pZCI6ImlzMTc0NjAiLCJwcm9kdWN0X2lkIjoiMSJ9fQ.NJ0yCml8QKdp1QqsVtH0jZFthw2KRADxfBfB2KYTInmgkZ3Q0EnGgykHZyUXgxfLsJMN7k7DhvDriUymFRcwdQ",
+  "user_token_lifetime" : 86400,
+  "user" : null
+}
+
+$done({body: JSON.stringify(obj)});
