5286db1b8590ec2b8245e21488d92005d4990559
Laila Azmi <lailaazmi@my.com>
2022-12-06 05:28:00 +0800

    lailaazmi

---
+++ Json/faceapp6.js
@@ -0,0 +1,12 @@
+let headers = $response.headers; 
+let obj = JSON.parse($response.body);
+ 
+obj = {"subscription_apple":{"subscription_exp":1658649180,"product_id":"y"}};  
+
+delete headers['X-FaceApp-ErrorCode'];
+ 
+$done({
+    body: JSON.stringify(obj),
+    headers: headers,
+    status: 200,
+});
