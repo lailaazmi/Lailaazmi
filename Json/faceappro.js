3581a01f98f78a1680807ec842858471bcf808f2
Laila Azmi <lailaazmi@my.com>
2022-12-06 06:06:11 +0800

    lailaazmi

---
+++ Json/faceappro.js
@@ -0,0 +1,29 @@
+/*
+
+*******************************
+ 
+Fitur Skrip: FaceApp Pro Seumur Hidup
+Versi perangkat lunak: Terbaru
+Penulis naskah: Lailaazmi 💞
+Waktu pembaruan: 7 Des 2022
+Umpan balik : twitter @nandhaazmi
+Grup umpan balik TG: https://t.me/nandhaazmi
+Grup TG: https://t.me/IDGratis
+Pernyataan penggunaan: ⚠️⚠️ Script ini hanya untuk pembelajaran dan komunikasi, tolong jangan cetak ulang dan jual! ⚠️⚠️
+
+*******************************
+
+*/
+
+let headers = $response.headers; 
+let obj = JSON.parse($response.body);
+ 
+obj = {"subscription_apple":{"subscription_exp":32356792106,"product_id":"y"}};  
+
+delete headers['X-FaceApp-ErrorCode'];
+ 
+$done({
+    body: JSON.stringify(obj),
+    headers: headers,
+    status: 200,
+});
