9884b0fbb04c1d0afe014f59bd2e1170fc37ed5d
Laila Azmi <lailaazmi@my.com>
2022-12-07 23:35:20 +0800

    lailaazmi

---
+++ Json/picseew.tes.js
@@ -0,0 +1,26 @@
+{
+  "receipt" : {
+    "receipt_type" : "Production",
+    "app_item_id" : 1208145167,
+    "receipt_creation_date" : "2022-12-07 15:18:15 Etc/GMT",
+    "bundle_id" : "com.sugarmo.ScrollClip",
+    "original_purchase_date" : "2021-01-13 16:42:48 Etc/GMT",
+    "in_app" : [
+
+    ],
+    "adam_id" : 1208145167,
+    "receipt_creation_date_pst" : "2022-12-07 07:18:15 America/Los_Angeles",
+    "request_date" : "2022-12-07 15:18:18 Etc/GMT",
+    "request_date_pst" : "2022-12-07 07:18:18 America/Los_Angeles",
+    "version_external_identifier" : 852688658,
+    "request_date_ms" : "1670426298967",
+    "original_purchase_date_pst" : "2021-01-13 08:42:48 America/Los_Angeles",
+    "application_version" : "3206",
+    "original_purchase_date_ms" : "1610556168000",
+    "receipt_creation_date_ms" : "1670426295000",
+    "original_application_version" : "3093",
+    "download_id" : 74059754708099
+  },
+  "status" : 0,
+  "environment" : "Production"
+}
