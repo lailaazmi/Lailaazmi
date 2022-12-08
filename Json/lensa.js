ddf3f6fa3b65190ab25391c39e7d1dbb8042b629
Laila Azmi <lailaazmi@my.com>
2022-12-08 11:43:14 +0800

    lailaazmi

--- Json/lensa.js
+++ Json/lensa.js
@@ -1,5 +1,22 @@
-var obj = JSON.parse($response.body); 
-obj['is_valid'] = true;
-obj['is_trial'] = true;
-obj['expiration_date'] = '2970-01-01T00:00:00Z';
+var obj = JSON.parse($response.body);
+
+obj={
+  "status": "ok",
+  "is_valid": true,
+  "expiration_date": "2099-12-01T03:51:32Z",
+  "is_trial": false,
+  "processing_count": 5,
+  "is_introductory_used": true,
+  "product_id": "",
+  "promotional_offer_id": "",
+  "is_grace_period": false,
+  "auto_renew_enabled": true,
+  "is_in_billing_retry_period": false,
+  "subscription_type": "annual",
+  "platform": "ios",
+  "device_user_info": {
+    "auth_type": "",
+    "subscription_valid": true
+  }
+}
 $done({body: JSON.stringify(obj)});
