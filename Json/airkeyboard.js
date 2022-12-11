4d8a00ca4de2e5729c883b752d2f65a9d9dfdf98
Laila Azmi <lailaazmi@my.com>
2022-12-11 09:40:40 +0800

    lailaazmi

--- Json/airkeyboard.js
+++ Json/airkeyboard.js
@@ -64,8 +64,39 @@ var objc = JSON.parse($response.body);
     "original_application_version" : "1.0.1.2",
     "download_id" : 74046545360586
   },
+  "pending_renewal_info" : [
+    {
+      "product_id" : "co.airapps.translatorkeyboard.yearly",
+      "original_transaction_id" : "350001106280498",
+      "auto_renew_product_id" : "co.airapps.translatorkeyboard.yearly",
+      "auto_renew_status" : "1"
+    }
+  ],
   "status" : 0,
-  "environment" : "Production"
-}
+  "environment" : "Production",
+  "latest_receipt_info" : [
+    {
+      "quantity" : "1",
+      "purchase_date_ms" : "1643256036000",
+      "expires_date" : "2995-05-07 09:09:09 Etc/GMT",
+      "expires_date_pst" : "2995-05-07 09:09:09 America/Los_Angeles",
+      "is_in_intro_offer_period" : "false",
+      "transaction_id" : "350001106280498",
+      "is_trial_period" : "true",
+      "original_transaction_id" : "350001106280498",
+      "purchase_date" : "2022-01-27 04:00:36 Etc/GMT",
+      "product_id" : "co.airapps.translatorkeyboard.yearly",
+      "original_purchase_date_pst" : "2022-01-26 20:00:37 America/Los_Angeles",
+      "in_app_ownership_type" : "PURCHASED",
+      "subscription_group_identifier" : "20606634",
+      "original_purchase_date_ms" : "1643256037000",
+      "web_order_line_item_id" : "350000493595908",
+      "expires_date_ms" : "32356801608000",
+      "purchase_date_pst" : "2022-01-26 20:00:36 America/Los_Angeles",
+      "original_purchase_date" : "2022-01-27 04:00:37 Etc/GMT"
+    }
+  ],
+  "latest_receipt" : "5L2c6ICF77ya5rWl6L275bCYIAoK5pu05aSa56C06Kej6KeE5YiZ6I635Y+W77yaCgpUR+e+pOe7hO+8mmh0dHBzOi8vdC5tZS95cWNfNzc3ClRH6aKR6YGT77yaaHR0cHM6Ly90Lm1lL3lxY18xMjMKCuKaoO+4j+KaoO+4j+KaoO+4j+a1pei9u+WwmENyYWNrIOS7heS+m+WtpuS5oOS4juS6pOa1gSDor7fli7\/ovazovb3kuI7otKnljZbvvIE="
+};
 
 $done({ body : JSON.stringify(objc) });
