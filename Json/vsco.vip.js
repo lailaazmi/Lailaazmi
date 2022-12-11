3d166175b79168f8ad15da85098505d3e30092ab
Laila Azmi <lailaazmi@my.com>
2022-12-11 23:32:27 +0800

    lailaazmi

---
+++ Json/vsco.vip.js
@@ -0,0 +1,43 @@
+var obj = JSON.parse($response.body);
+
+obj={
+  "request_date_ms" : 1622445760393,
+  "request_date" : "2021-05-31T07:22:40Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+
+    },
+    "first_seen" : "2021-05-31T07:18:40Z",
+    "original_application_version" : "19615",
+    "other_purchases" : {
+
+    },
+    "management_url" : "itms-apps://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "VSCOANNUAL" : {
+        "is_sandbox" : false,
+        "period_type" : "normal",
+        "billing_issues_detected_at" : null,
+        "unsubscribe_detected_at" : null,
+        "expires_date" : "2090-07-16T12:15:53Z",
+        "grace_period_expires_date" : null,
+        "original_purchase_date" : "2019-07-02T12:15:54Z",
+        "purchase_date" : "2019-07-02T12:15:53Z",
+        "store" : "app_store"
+      }
+    },
+    "entitlements" : {
+      "membership" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2020-11-05T07:03:11Z",
+        "product_identifier" : "VSCOANNUAL",
+        "expires_date" : "2090-12-05T07:03:11Z"
+      }
+    },
+    "original_purchase_date" : "2020-12-28T17:41:34Z",
+    "original_app_user_id" : "139535280",
+    "last_seen" : "2021-05-31T07:18:40Z"
+  }
+}
+
+$done({body: JSON.stringify(obj)});
